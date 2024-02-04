79ee3cfd18323d276801b9d8e81bbc9957f7ff7d
kirat14 <moumini.tarik@gmail.com>
2024-02-04 15:59:25 -0600

    save

--- src/app.js
+++ src/app.js
@@ -2,185 +2,40 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './scss/style.scss';
-
-
-// In your entry file or module
-function importAll(r) {
-	return r.keys().map(r);
-  }
-  
-//const images = importAll(require.context('./img', false, /\.(jpg|jpeg|gif|png|svg|webp)$/));
-  
-
-
-// load project dynamically from json file
-// use template.html as a loop template
-document.addEventListener('DOMContentLoaded', function () {
-	var dynamic_content_container = document.getElementById('dynamic-portfolio-container');
-
-	// Your template file path
-	var templateFilePath = './template.html';
-
-
-	// Create a new XMLHttpRequest object for the template file
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', templateFilePath, true);
-
-	// Set up a callback function to handle the template file loading
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-
-			// Replace placeholders with data from the current item
-			var dynamicContent = xhr.responseText
-
-			// Your JSON file path
-			var jsonFilePath = './data/projects.json';
-
-			// Create a new XMLHttpRequest object for the JSON file
-			var jsonXhr = new XMLHttpRequest();
-			jsonXhr.open('GET', jsonFilePath, true);
-
-			// Set up a callback function to handle the JSON file loading
-			jsonXhr.onreadystatechange = function () {
-				if (jsonXhr.readyState === 4 && jsonXhr.status === 200) {
-					// Parse the JSON data
-					var jsonData = JSON.parse(jsonXhr.responseText);
-
-					 // Array to store image elements
-					 var imageElements = [];
-
-					// Loop through each item in the JSON data
-					jsonData.forEach(function (item) {
-						// Create a new image element
-						var thumbnail_image = new Image();
-						var hd_image = new Image();
-						// Attach onload event to the image
-						thumbnail_image.onload = function () {
-							// Image loaded successfully
-							console.log('Image loaded:', item.thumbnail_name);
-						};
-						hd_image.onload = function () {
-							// Image loaded successfully
-							console.log('Image loaded:', item.hd_name);
-						};
-						// Set image source
-						thumbnail_image.src = './img/' + item.thumbnail_name;
-						// Store the image element in the array
-						imageElements.push(thumbnail_image);
-
-						// Set image source
-						hd_image.src = './img/' + item.hd_name;
-						// Store the image element in the array
-						imageElements.push(hd_image);
-
-						dynamicContent = xhr.responseText;
-						dynamicContent = dynamicContent
-							.replace('{{tumbnail_image}}', item.thumbnail_name)
-							.replace('{{hd_image}}', item.hd_name)
-							.replace('{{project_description}}', item.project_description)
-							.replaceAll('{{name}}', item.name);
-
-						// Append the dynamic content to the container
-						dynamic_content_container.innerHTML += dynamicContent;
-					});
-
-					// Function to check if all images are loaded
-					function allImagesLoaded() {
-						return imageElements.every(function (image) {
-							return image.complete;
-						});
-					}
-
-					// Check if all images are loaded
-					function checkImages() {
-						if (allImagesLoaded()) {
-							// All images are loaded
-							isotop_init();
-						} else {
-							// Not all images are loaded, check again after a delay
-							setTimeout(checkImages, 100);
-						}
-					}
-
-					// Check images initially
-					checkImages();
-
-					
-				}
-			};
-			// Send the request for the JSON file
-			jsonXhr.send();
-		}
-	};
-	// Send the request for the template file
-	xhr.send();
-
-});
+// import Swiper JS
+import Swiper from 'swiper';
 
 
 
-function isotop_init() {
-	// external js: isotope.pkgd.js
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	slidesPerView: 1	,
+	spaceBetween: 16,
 
-	// init Isotope
-	var iso = new Isotope('.grid', {
-		itemSelector: '.element-item',
-		layoutMode: 'fitRows'
-	});
-
-	// filter functions
-	var filterFns = {
-		// show if number is greater than 50
-		numberGreaterThan50: function (itemElem) {
-			var number = itemElem.querySelector('.number').textContent;
-			return parseInt(number, 10) > 50;
+	// Responsive breakpoints
+	breakpoints: {
+		// when window width is >= 320px
+		576: {
+			slidesPerView: 2
 		},
-		// show if name ends with -ium
-		ium: function (itemElem) {
-			var name = itemElem.querySelector('.name').textContent;
-			return name.match(/ium$/);
+		// when window width is >= 480px
+		768: {
+			slidesPerView: 3
+		},
+		// when window width is >= 640px
+		992: {
+			slidesPerView: 4
 		}
-	};
+	},
 
-	// bind filter button click
-	var filtersElem = document.querySelector('.filters-button-group');
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
 
-	filtersElem.addEventListener('click', function (event) {
-		event.preventDefault();
-
-		if (!matchesSelector(event.target, 'a')) {
-			return;
-		}
-
-		var filterValue = event.target.getAttribute('data-filter');
-		// use matching filter function
-		filterValue = filterFns[filterValue] || filterValue;
-		iso.arrange({ filter: filterValue });
-
-	});
-
-	// change is-checked class on buttons
-	var buttonGroups = document.querySelectorAll('.filters-button-group');
-	for (var i = 0, len = buttonGroups.length; i < len; i++) {
-		var buttonGroup = buttonGroups[i];
-		radioButtonGroup(buttonGroup);
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
 	}
-
-	let view_all = document.getElementById('view-all');
-	view_all.addEventListener('click', function (event) {
-		event.preventDefault();
-		document.getElementById('nav-link-all').click();
-	});
-
-	function radioButtonGroup(buttonGroup) {
-		buttonGroup.addEventListener('click', function (event) {
-			// only work with buttons
-			if (!matchesSelector(event.target, 'a')) {
-				return;
-			}
-			buttonGroup.querySelector('.active').classList.remove('active');
-			event.target.classList.add('active');
-		});
-	}
-
-}
+});
